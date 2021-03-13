a757d06bed9c3a064c116b72da24e5d0f7613ea7
agavrel <gavrel.antonin@gmail.com>
2021-03-13 14:56:35 -0500

    [ADD] Rust unsafely working like a charm

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-### Simple UDP server/client in Rust
+### Large file transfer over UDP, server/client in Rust
 
 #### About [User Data Protocol](https://en.wikipedia.org/wiki/User_Datagram_Protocol)
 
@@ -7,11 +7,18 @@
 ##### UDP max data length
 
 UDP packet is limited to 64kB (65536), however we need to take into account that each UDP's packet  
-also has a UDP header of 8 bytes as well as an IP header of 20 bytes. Hence MAX_DATA_LENGTH is  
-limited to 65,507 bytes, see the following code:
+also has a UDP header of 8 bytes as well as an IP header of 20 bytes. Hence `MAX_DATA_LENGTH` is limited to 65,507 bytes.
 
 ```rs
 const UDP_HEADER: u16 = 8;
 const IP_HEADER: u16 = 20;
 const MAX_DATA_LENGTH: u16 = 64 * 1024 - UDP_HEADER - IP_HEADER;
 ```
+
+We have to remove bytes from our custom header which decrease our `MAX_CHUNK_SIZE` further by a few bytes (storing total number, index and filename):
+```rs
+const MAX_CHUNK_SIZE = MAX_DATA_LENGTH - AG_HEADER
+```
+
+**The following Program can send file up to about 4Gb** *(65535 (u16) chunks multiplied by chunk_size and divided by 1024^3 to convert from bytes to Gb).*  
+This can be extended to much higher limits simply by using extra bytes in the custom header `AG_HEADER`.  
--- udp-server.rs
+++ udp-server.rs
@@ -20,10 +20,12 @@ fn memcpy(dst_ptr:*mut u8, src_ptr:*const u8, len:usize) {
     }
 }
 
+#[inline(always)]
 fn next_power_of_two(n:u32) -> u32 {
     return 32 - (n - 1).leading_zeros();
 }
 
+#[inline(always)]
 fn write_chunks_to_file(filename: &str, bytes:&[u8]) -> Result<bool, io::Error> {
     let mut file = File::create(filename)?;
     file.write_all(bytes)?;
@@ -37,69 +39,32 @@ fn main() {
     let mut count = 0;
     let mut chunks_cnt:u32 = 0xffff;
     let mut total_size:usize = 0;
-   // let mut bytes_buf:*mut u8;
     let mut layout;
     unsafe { layout = MaybeUninit::<Layout>::uninit().assume_init(); };
 
     let mut bytes_buf;
     unsafe { bytes_buf = MaybeUninit::<*mut u8>::uninit().assume_init(); };
-    //let bytes_buf = unsafe {let mut bytes_buf = MaybeUninit::<*mut u8>::uninit();};
-
 
     loop {
-        let mut buf = [0u8; MAX_CHUNK_SIZE];
+        let mut buf = [0u8; MAX_CHUNK_SIZE + AG_HEADER];
         let sock = socket.try_clone().expect("Failed to clone socket");
-
-        //bytes_buf = Vec::new();
         match socket.recv_from(&mut buf) {
             Ok((size, src)) => { // thanks https://doc.rust-lang.org/beta/std/net/struct.UdpSocket.html#method.recv_from
                 total_size += size;
                 let packet_index:usize = (buf[0] as usize) << 8 | buf[1] as usize;
                 if count == 0 {
                     chunks_cnt = (buf[2] as u32) << 8 | buf[3] as u32;
-                    //bytes_buf = vec![0; MAX_CHUNK_SIZE * chunks_cnt as usize];
                     let n:usize = 0x10000 << next_power_of_two(chunks_cnt);
                    // assert_eq!(n.count_ones(), 1); // can check with this function that n is aligned on power of 2
                     unsafe {
                          layout = Layout::from_size_align_unchecked(n, mem::align_of::<u8>());
                          bytes_buf = alloc(layout);
-
-
-
-                        //*(bytes_buf as *mut u8) = 42;
-                       // assert_eq!(*(ptr as *mut u16), 42);
                     }
-
-                    println!("Memory allocated: {:x}", n);
-
-                /*   bytes_buf = Vec::with_capacity(n);
-                    unsafe { bytes_buf.set_len(n); }*/
                 }
-
-                 unsafe { let dst_ptr = bytes_buf.offset((packet_index*MAX_CHUNK_SIZE) as isize);
-                      //     let src_ptr = buf.offset((AG_HEADER) as isize);
-                        memcpy(dst_ptr, &buf[AG_HEADER], size-AG_HEADER); // TODO: convert &buf[AG_HEADER] to PTR
-                  };
-
-                /*
-                if packet_index == 0 { // make sure got the very first chunk
-                    println!("First packet"); //
-                    unsafe {
-                        let bytes =  std::slice::from_raw_parts(bytes_buf, total_size);
-                        println!("File bytes{:?}", &bytes); //
-                    };
-
-                 }*/
-               // let bytes = unsafe { std::slice::from_raw_parts(bytes_buf, total_size) };
-                 //           println!("File bytes{:?}", bytes);
-                /*
                 unsafe {
-                    let dst_ptr = &mut bytes_buf[packet_index*MAX_CHUNK_SIZE] as *mut u8;
-                    let src_ptr = &buf[AG_HEADER] as *const u8;
-                    std::ptr::copy_nonoverlapping(src_ptr, dst_ptr, size-AG_HEADER);
-                }*/
-                //bytes_buf[packet_index*MAX_CHUNK_SIZE..packet_index*MAX_CHUNK_SIZE+size-AG_HEADER].copy_from_slice(&buf[AG_HEADER..size]);
-
+                    let dst_ptr = bytes_buf.offset((packet_index*MAX_CHUNK_SIZE) as isize);
+                    memcpy(dst_ptr, &buf[AG_HEADER], size-AG_HEADER);
+                };
                 thread::spawn(move || {
                     //let s = String::from_utf8_lossy(&buf);
                     println!("receiving packet {} from: {}", packet_index, src);
@@ -114,8 +79,7 @@ fn main() {
         }
          if count == chunks_cnt { // all chunks have been collected, write bytes to file
             let bytes = unsafe { std::slice::from_raw_parts(bytes_buf, total_size) };
-            //println!("File bytes{:?}", bytes); ///
-            let result = write_chunks_to_file(filename, &bytes); // [0x34,0x32].to_vec()
+            let result = write_chunks_to_file(filename, &bytes);
             match result {
                 Ok(true) => println!("Succesfully created file: {}", true),
                 Ok(false) => println!("Could not create file: {}", false),
